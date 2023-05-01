9300b3ca538e0618e03fc82610d6338fa672d72d
Mergen <131531589+kurmaann@users.noreply.github.com>
2023-05-01 18:02:36 +0600

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,12 @@
+value = Math.PI;
+console.log(value.toFixed(2));
+
+value = Math.min(15, 11, 16, 12, 51, 12, 13, 51);
+value = Math.max(15, 11, 16, 12, 51, 12, 13, 51);
+console.log(value);
+
+value = Math.random();
+console.log(value.toFixed(2));
+
+value = Math.random();
+console.log(value.toFixed(1));
